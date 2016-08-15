c8e39602f589ce52ffcc0d00ccf5cae9e689270a
sirmarbug <maly95_pl@o2.pl>
2016-08-15 22:00:56 +0200

    0.0.2
    Uzupełnienie pliku Karta.java

--- src/oczko/Karta.java
+++ src/oczko/Karta.java
@@ -10,5 +10,28 @@ package oczko;
  * @author mariuszbugajski
  */
 public class Karta {
+    int typ, nr, wartosc;
     
+    public Karta(){
+        this.typ = 0;
+        this.nr = 0;
+        this.wartosc = 0;
+    }
+    
+    public Karta(int i){
+        Random r = new Random();
+        this.typ = r.nextInt(4);
+        this.nr = r.nextInt(12)+2;
+        if(this.nr == 11){
+            this.wartosc = 2;
+        }else if(this.nr == 12){
+            this.wartosc = 3;
+        }else if(this.nr == 13){
+            this.wartosc = 4;
+        }else if(this.nr == 14){
+            this.wartosc = 11;
+        }else{
+            this.wartosc = this.nr;
+        }
+
 }
